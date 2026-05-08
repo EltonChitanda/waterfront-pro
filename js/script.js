035ddb3ae9149b6343acf4d4f0d6064c27be0e23
EltonChitanda <eltonc.webdev@gmail.com>
2026-05-08 11:09:37 +0200

    initial website build

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const scrollTop = document.getElementById('scrollTop');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 300) {
+        scrollTop.classList.add('visible');
+    } else {
+        scrollTop.classList.remove('visible');
+    }
+});

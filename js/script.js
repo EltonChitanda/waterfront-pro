77d7ccbb880498c56f1d340081c7100a65a429dd
EltonChitanda <eltonc.webdev@gmail.com>
2026-05-08 12:17:28 +0200

    navbar edit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+// Active nav link
+const currentPage = window.location.pathname.split('/').pop();
+const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+
+navLinks.forEach(link => {
+    const linkPage = link.getAttribute('href');
+    if (linkPage === currentPage || (currentPage === '' && linkPage === 'index.html')) {
+        link.classList.add('active');
+    } else {
+        link.classList.remove('active');
+    }
+});
+
+// Scroll to top button
 const scrollTop = document.getElementById('scrollTop');
 
 window.addEventListener('scroll', () => {
